cf6e9fe6f111d0d02009889b280bb23bd51ffe44
jwill-8 <jwilliams_388@icloud.com>
2025-07-07 15:40:54 -0400

    simple fetching program to learn about API calls

--- JS/api-practice/script.js
+++ JS/api-practice/script.js
@@ -1,3 +1,5 @@
+const API_KEY = "YWfo3UVAmQIvnh8TCcKbHtiWsrRtKQlk";
+
 function toURL(toSearch) {
   return (
     "https://api.giphy.com/v1/gifs/translate?api_key=" +
@@ -7,16 +9,13 @@ function toURL(toSearch) {
   );
 }
 
-const API_KEY = "YWfo3UVAmQIvnh8TCcKbHtiWsrRtKQlk";
-
 let url;
-
+const search = document.querySelector("#search");
 const img = document.querySelector("img");
-
 const searchButton = document.querySelector("#search-button");
 
 searchButton.addEventListener("click", () => {
-  url = toURL(document.querySelector("#search"));
+  url = toURL(search.value);
   fetch(url, { mode: "cors" })
     .then(function (response) {
       // initial promise resolution returns another promise
@@ -26,5 +25,9 @@ searchButton.addEventListener("click", () => {
       // this returns an obj that contains the img we need to append
       //console.log(response.data.images.original.url);
       img.src = response.data.images.original.url;
+    })
+    .catch(function (error) {
+      console.error("Error fetching from Giphy API:", error);
+      img.alt = "Could not load image.";
     });
 });
