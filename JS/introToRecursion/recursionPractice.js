33cab43c7bba54605a43e8054f405135c6916c25
jwill-8 <jwilliams_388@icloud.com>
2025-08-05 16:38:55 -0400

    linked list

--- JS/introToRecursion/recursionPractice.js
+++ JS/introToRecursion/recursionPractice.js
@@ -36,3 +36,35 @@ function fib(n) {
 }
 
 console.log(fib(7));
+
+let list = {
+  value: 1,
+  next: {
+    value: 2,
+    next: {
+      value: 3,
+      next: {
+        value: 4,
+        next: null,
+      },
+    },
+  },
+};
+
+function printListRecursive(list) {
+  console.log(list.value);
+  if (list.next) {
+    printListRecursive(list.next);
+  }
+}
+
+//printListRecursive(list);
+
+function printListRecursiveBackwards(list) {
+  if (list.next) {
+    printListRecursiveBackwards(list.next);
+  }
+  console.log(list.value);
+}
+
+printListRecursiveBackwards(list);
